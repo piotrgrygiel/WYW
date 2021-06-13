df7f39914a15c4f9ca2ed34b4d799d98baf64719
MacWen <maciej.wen@gmail.com>
2021-06-13 10:41:44 +0200

    zmieniony regex

--- WYW/WYW/inputModel.cs
+++ WYW/WYW/inputModel.cs
@@ -4,13 +4,13 @@ namespace WYW
     public class inputModel
     {
         [Required]
-        [RegularExpression(@"^[A-Z]{3}\d{3,4}$", ErrorMessage = "Wrong Flight No.")]
+        [RegularExpression(@".*\d{3,4}$", ErrorMessage = "Wrong Flight No.")]
         public string Name { get; set; }
         private int iataLength = 2;
 
         public string getNumber()
         {
-            string number = Name.Substring(iataLength);
+            string number = Name.Substring(iataLength+1);
             for(int i=0; i < number.Length; i++)
             {
                 if(System.Char.IsDigit(number[i]))
