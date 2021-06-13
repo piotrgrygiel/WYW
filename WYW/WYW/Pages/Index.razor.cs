12449b0ae8767442d63ecad79b892b3fd1e86b2a
wujaadam87 <wuja.adam@gmail.com>
2021-06-13 12:36:11 +0200

    time to departure implemented

--- WYW/WYW/Pages/Index.razor.cs
+++ WYW/WYW/Pages/Index.razor.cs
@@ -2,17 +2,15 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
-using WYW;
+using System.Threading;
 using WYW.Data;
 
 namespace WYW.Pages
 {
-    public partial class Index
+    public partial class Index : IDisposable
     {
         [Inject]
-        ILogger<InputModel> Logger {get; set;}
-
-        private InputModel inputfdModel = new InputModel();
+        private ILogger<InputModel> Logger {get; set;}
 
         [Inject]
         private IApiResponseService ApiService { get; set; }
@@ -22,6 +20,9 @@ namespace WYW.Pages
 
         private FlightInfo flightInfo = null;
         private bool userFilledFlightNumber = false;
+        private InputModel inputfdModel = new InputModel();
+        private TimeSpan TimeToDeparture;
+        private Timer timer = null;
 
         private void HandleValidSubmit()
         {
@@ -30,6 +31,27 @@ namespace WYW.Pages
             userFilledFlightNumber = true;
 
             flightInfo = ApiService.RecentResponse.LastResponse.FirstOrDefault(flight => flight.flight.number.Equals(inputfdModel.GetNumber(), StringComparison.InvariantCultureIgnoreCase));
+
+            if (flightInfo != null && timer == null)
+            {
+                timer = new Timer((e) => { UpdateTimeToDeparture(); }, null, TimeSpan.Zero, TimeSpan.FromSeconds(60));
+            }
+        }
+
+        private void UpdateTimeToDeparture()
+        {
+            var now = DateTimeService.Now();
+            var departureTime = flightInfo == null ? now : flightInfo.departure.scheduledTime.ToLocalTime();
+
+            TimeToDeparture = departureTime - now;
+
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            if (timer != null)
+                timer.Dispose();
         }
     }
 }
