a76baa0b55cee9d31b6b571a6f5b77cc10c179e3
MacWen <maciej.wen@gmail.com>
2021-06-13 10:40:56 +0200

    poprawka zgloszona przy code review

--- WYW/WYW/inputModel.cs
+++ WYW/WYW/inputModel.cs
@@ -4,13 +4,23 @@ namespace WYW
     public class inputModel
     {
         [Required]
-        [RegularExpression(@"^[A-Z]{2}\d{3,4}$", ErrorMessage = "Wrong Flight No.")]
+        [RegularExpression(@"^[A-Z]{3}\d{3,4}$", ErrorMessage = "Wrong Flight No.")]
         public string Name { get; set; }
         private int iataLength = 2;
 
         public string getNumber()
         {
-            return Name.Substring(iataLength);
+            string number = Name.Substring(iataLength);
+            for(int i=0; i < number.Length; i++)
+            {
+                if(System.Char.IsDigit(number[i]))
+                {
+                    number = number.Substring(i);
+                    break;
+                }
+            }
+
+            return number;
         }
     }
 }
